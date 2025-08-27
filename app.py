571c3ba24a8f95fd46467123c4c586da3959ee62
rezvanovpavellaba <128293581+rezvanovpavellaba@users.noreply.github.com>
2025-08-27 14:18:23 +0300

    Добавил модуль нейро до граф включ

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
-import pandas as pd
 import streamlit as st
-from io import BytesIO
 from utils.utils import *
 from utils.metabolomics import *
 from utils.cardiotoxicity import *
 from utils.neurotoxicity import *
 from utils.hepatotoxicity import *
 
+st.set_page_config(page_title="Органотоксичность", layout="wide")
 st.title("Органотоксичность")
 
 panel = main_radio_button_study()
 
-# Инициализация session state для fc_mode, если его еще нет
-if "fc_mode" not in st.session_state:
-    st.session_state["fc_mode"] = 'ratio (B/A)'  # Значение по умолчанию
+
 
 if panel == "Метаболомика":
    metabolomika_app()
 elif panel == "Кардиотоксичность":
    st.subheader("Кардиотоксичность")
 elif panel == "Нейротоксичность":
-   st.subheader("Нейротоксичность")
+   neurotoxicity_app()
 else:
    st.subheader("Гепатотоксичность")
